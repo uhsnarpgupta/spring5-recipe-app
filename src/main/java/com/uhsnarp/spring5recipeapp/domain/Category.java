97406fb01a4a1fd5614818ca8718d661dfe15aac
pranshu <uhsnarpggupta@gmail.com>
2018-11-22 10:07:32 +0530

    Updated

---
+++ src/main/java/com/uhsnarp/spring5recipeapp/domain/Category.java
@@ -0,0 +1,39 @@
+package com.uhsnarp.spring5recipeapp.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    @ManyToMany
+    private Set<Recipe> recipes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipe() {
+        return recipes;
+    }
+
+    public void setRecipe(Set<Recipe> recipe) {
+        this.recipes = recipes;
+    }
+}
--- src/main/java/com/uhsnarp/spring5recipeapp/domain/Recipe.java
+++ src/main/java/com/uhsnarp/spring5recipeapp/domain/Recipe.java
@@ -16,8 +16,6 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //todo add
-    //private Difficulty difficulty ;
 
     @Lob
     private Byte[] image;
@@ -27,6 +25,12 @@ public class Recipe {
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty ;
+
+    @ManyToMany
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
@@ -114,5 +118,21 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
